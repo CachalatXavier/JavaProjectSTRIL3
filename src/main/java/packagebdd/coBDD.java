8830e0be6ca32bd8a772b89fef682c0472dcdaf7
CachalatXavier <xavier.cachalat@gmail.com>
2016-05-27 09:45:14 +0200

    Merge origin/master
    Conflicts:
    	src/main/java/packagebdd/insertBDD.java

--- src/main/java/packagebdd/coBDD.java
+++ src/main/java/packagebdd/coBDD.java
@@ -27,7 +27,7 @@ public class coBDD {
             System.out.println("Connexion Fail..." + cnfe);
         }
         
-        String url = "jdbc:mysql://localhost:8889/javaBDD?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
+        String url = "jdbc:mysql://localhost:3306/javaBDD?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
 
         //François: 8889 javaBDD root root
         //Xavier: 3306 javabdd root
@@ -35,7 +35,7 @@ public class coBDD {
         //Najim: 3306 javaBDD root root
         
         try {
-            DBConnection = (Connection) DriverManager.getConnection(url, "root", "root");
+            DBConnection = (Connection) DriverManager.getConnection(url, "root", "");
             System.out.println("Database Connected !");
         }
         catch (SQLException se){
